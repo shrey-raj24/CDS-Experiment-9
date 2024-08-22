b781c0db77132011145926aa81ea9cd13f225c01
Shrey raj <shreyraj37@gmail.com>
2024-08-23 01:17:36 +0530

    Add files via upload

---
+++ Shrey_B2_123_Exp9C_CDS.cpp
@@ -0,0 +1,36 @@
+// NAME - SHREY RAJ
+// PRN - 23070123123
+// EXPERIMENT - 9(C) 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *p ,b=10;
+    p = &b ;
+    cout <<*p << "  " << b << endl <<p <<"  "<< &b<<endl;
+    p++;
+    cout<<"After increment:" <<p<<endl;
+    float *n, a=8.923;
+    n=&a;
+    cout<< endl<<*n<<"  "<<a<<endl<<n<<"  "<<&a<<endl;
+    n++;
+    cout<<" After increment" <<n <<endl;
+    char *ch, c(10);
+    c='#';
+    ch=&c;
+    cout<< endl<< (void*)ch<<"  "<<endl;
+    ch++;
+    cout<<" After increment:" << (void*)ch<<endl;
+    return 0;
+}
+
+// 10  10
+// 0x61ff00  0x61ff00
+// After increment:0x61ff04
+// 
+// 8.923  8.923
+// 0x61fefc  0x61fefc
+//  After increment0x61ff00 
+// 0x61fefb
+//  After increment:0x61fefc
